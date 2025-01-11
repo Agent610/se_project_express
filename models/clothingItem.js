ab100159e5fdddd422a813ed672f582d386c1d03
Agent610 <BhavsarParthD96@Gmail.com>
2025-01-11 16:24:25 -0500

    ClothingItem

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -1,5 +1,35 @@
 const mongoose = require("mongoose");
 
-const clothingItemSchema = new mongoose.Schema({});
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    type: String,
+    required: true,
+    enum: [Weather, "Hot", "Warm", "Cold"],
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+  owner: {
+    type: ObjectId,
+    required: true,
+  },
+  likes: {},
+  createdAt: {
+    type: Date,
+  },
+});
 
 module.exports = mongoose.model("item", clothingItemSchema);
