42fb0787aa2a75cb8061cff9c3c2f5c17dfa88e4
Agent610 <BhavsarParthD96@Gmail.com>
2025-01-11 15:53:25 -0500

    User Schema

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const mongoose = require("mongoose");
 const app = express();
 const { PORT = 3001 } = process.env;
 
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db").then[
-  () => {
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
     console.log("Connected to DB");
-  }
-].catch(console.error);
+  })
+  .catch(console.error);
 
 app.listen(PORT, () => {
-  console.log("Server is running on port ${PORT}");
+  console.log(`Server is running on port ${PORT}`);
 });
