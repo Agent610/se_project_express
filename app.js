8aae16277009b8567cec000d1db2b15f12f770de
Agent610 <BhavsarParthD96@Gmail.com>
2025-03-17 21:15:59 -0400

    Repo

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-// const routes = require("./routes");
-const mainRouter = require("./routes/index");
-const { login, createUser } = require("./controllers/users");
+const routes = require("./routes");
 
 const app = express();
-const { PORT = 3002 } = process.env;
+const { PORT = 3001 } = process.env;
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
@@ -16,10 +14,8 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-app.post("/signin", login);
-app.post("/signup", createUser);
-app.use("/", mainRouter);
-// app.use(routes());
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT => ", PORT);
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
+const { login, createUser } = require("../controllers/users");
 const { NOT_FOUND } = require("../utils/errors");
 const clothingItem = require("./clothingItems");
-
 const userRouter = require("./users");
+
+router.post("/signin", login);
+router.post("/signup", createUser);
 
 router.use("/items", clothingItem);
 router.use("/users", userRouter);
+
 router.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "The page was not found" });
 });
