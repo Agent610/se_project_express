9290332966bdb7e365d48e7734a529759faba3b0
Agent610 <BhavsarParthD96@Gmail.com>
2025-04-08 19:33:39 -0400

    Sprint 15 Advanced middleware 1/3

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes");
 
+const errorHandler = require("./middlewares/error-handler");
+
 const app = express();
 const { PORT = 3001 } = process.env;
 
@@ -16,6 +18,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT => ", PORT);
--- utils/errors.js
+++ utils/errors.js
@@ -8,6 +8,46 @@ const REGISTRATION_ERROR = 409;
 const DEFAULT = 500;
 const ERROR = 11000;
 
+class BadRequestError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 400;
+    this.name = "BadRequestError";
+  }
+}
+
+class UnauthorizedError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 401;
+    this.name = "Unauthorized Error";
+  }
+}
+
+class ForbiddenError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 403;
+    this.name = "ForbiddenError";
+  }
+}
+
+class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 404;
+    this.name = "NotFoundError";
+  }
+}
+
+class ConflictError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 409;
+    this.name = "ConflictError";
+  }
+}
+
 module.exports = {
   SUCCESS,
   CREATE,
@@ -18,4 +58,9 @@ module.exports = {
   REGISTRATION_ERROR,
   DEFAULT,
   ERROR,
+  BadRequestError,
+  UnauthorizedError,
+  ForbiddenError,
+  NotFoundError,
+  ConflictError,
 };
